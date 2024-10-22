f796c14d0f0dc0923de475a199aa36a64bf8cc0d
dicey24 <dicede62@gmail.com>
2024-10-22 17:02:27 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# portfolio-2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dicey24/portfolio-2)
---
+++ src/pages/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skills: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 py-16 mt-16">
+      <h1 className="text-4xl font-bold mb-8">Skills</h1>
+      <p className="text-xl">This is the Skills page. Add your content here.</p>
+    </div>
+  );
+};
+
+export default Skills;
